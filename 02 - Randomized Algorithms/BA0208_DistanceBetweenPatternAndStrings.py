89a344c687e554f4eac1f0b100cceeb872c32ecc
Stephen Blatti <stephen.p.blatti@gmail.com>
2017-11-07 11:45:20 -0600

    Update

---
+++ 02 - Randomized Algorithms/BA0208_DistanceBetweenPatternAndStrings.py
@@ -0,0 +1,41 @@
+'''
+Compute DistanceBetweenPatternAndStrings
+
+Find the distance between a pattern and a set of strings.
+
+Given: A DNA string Pattern and a collection of DNA strings Dna.
+
+Return: DistanceBetweenPatternAndStrings(Pattern, Dna).
+'''
+# Sample data
+pattern = 'AAA'
+strings = ['TTACCTTAAC','GATATCTGTC','ACGGCGTTCG','CCCTAAAGAG','CGTCAGAGGT']
+#output = 5
+import HelperFunctions as hf
+
+def distanceBetweenPatternAndString(pattern, text):
+    ham_distance = float('inf')
+    for i in range(len(text) - len(pattern) + 1):
+        hd = hf.HammingDistance(pattern, text[i:i + len(pattern)])
+        if hd < ham_distance:
+            ham_distance = hd
+    return ham_distance
+
+
+def distanceBetweenPatternAndStrings(pattern, strings):
+    k = len(pattern)
+    distance = 0
+    for i in range(len(strings)):
+        distance += distanceBetweenPatternAndString(pattern, strings[i])
+    return distance
+
+
+
+if __name__ == '__main__':
+
+    infile = open('data/rosalind_ba2h.txt', 'r')
+    lines = infile.read().splitlines()
+    pattern = lines[0]
+    strings = lines[1].split(' ')
+    infile.close()
+    print(distanceBetweenPatternAndStrings(pattern, strings))
--- 02 - Randomized Algorithms/README.md
+++ 02 - Randomized Algorithms/README.md
@@ -9,7 +9,7 @@ Implement MotifEnumeration to find all (k, d)-motifs in a collection of strings.
 Given: Integers k and d, followed by a collection of strings Dna.
 
 Return: All (k, d)-motifs in Dna.
-
+___
 ###### 02 Median String Problem
 
 Find a median string.
@@ -17,7 +17,7 @@ Find a median string.
 Given: An integer k and a collection of strings Dna.
 
 Return: A k-mer Pattern that minimizes d(Pattern, Dna) over all k-mers Pattern.
-
+___
 ###### 03 Profile-most Probable k-mer Problem
 
 Find a Profile-most probable k-mer in a string.
@@ -25,5 +25,20 @@ Find a Profile-most probable k-mer in a string.
 Given: A string Text, an integer k, and a 4 × k matrix Profile.
 
 Return: A Profile-most probable k-mer in Text.
+___
+###### 04
+___
+###### 05
+___
+###### 06
+___
+###### 07
+___
+###### 08 Compute DistanceBetweenPatternAndStrings
 
-###### 04 
+Find the distance between a pattern and a set of strings.
+
+Given: A DNA string Pattern and a collection of DNA strings Dna.
+
+Return: DistanceBetweenPatternAndStrings(Pattern, Dna).
+___
